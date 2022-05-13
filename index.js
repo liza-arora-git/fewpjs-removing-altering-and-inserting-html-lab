e686c3456555d25fb1a535d1323e61cfee42681c
Liza Arora <liza.arora@outlook.com>
2022-05-14 01:41:42 +1000

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let tempMain = document.getElementById('main');
+tempMain.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.className = "victory";
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Liza is the champion";
